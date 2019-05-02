0c6662611fe6dcdf8da6b7667f8f8760c0ab3f19
Jerry Lin <jerrylin1217@gmail.com>
2019-05-01 21:42:29 -0400

    changed light script triggers to sun elevation

--- python_scripts/manual_flux.py
+++ python_scripts/manual_flux.py
@@ -5,14 +5,15 @@ LIGHTS = [
 
 WARM_GLOW_EVENING = datetime.time(22,00,00)
 WARM_GLOW_MORNING = datetime.time(5,00,00)
+ELEVATION = 6
 
 now = datetime.datetime.now().time()
 
-sun_state = hass.states.get("sun.sun").state
+sun_elevation = hass.states.get("sun.sun").attributes['elevation']
 
-if (sun_state == 'above_horizon'):
+if (sun_elevation > ELEVATION):
     temp = 4000
-elif(now < WARM_GLOW_MORNING or now > WARM_GLOW_EVENING):
+elif(now < WARM_GLOW_MORNING or WARM_GLOW_EVENING < now):
     temp = 2200
 else:
     temp = 2700
